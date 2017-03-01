bc77f3ecd5c56727811d2804ebdbeb3e3c657868
Alejandro Santander <palebluedot@gmail.com>
2017-03-01 13:41:24 -0300

    Change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,15 +14,12 @@ function doCompile {
 }
 
 echo "Generating docs..."
+echo "ls -la"
 ls -la
+echo "ls -la node_modules"
 ls -la node_modules
+echo "ls -la node_modules/@awayjs"
 ls -la node_modules/@awayjs
-
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping docs deploy."
-    exit 0
-fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
@@ -68,7 +65,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
